b027d5618515075a0352787117cbe1358697fdfc
Krishna Khong <krishnakhong@gmail.com>
2023-10-23 12:28:45 +1100

    feat: added toggleterm

--- lua/krish/plugins/colorscheme.lua
+++ lua/krish/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-	{
-		"ellisonleao/gruvbox.nvim",
-		name = "gruvbox",
-		priority = 1000,
-		config = function()
-      vim.o.background = "dark"
-			vim.cmd([[colorscheme gruvbox]])
-		end
-	},
+    {
+        "ellisonleao/gruvbox.nvim",
+        name = "gruvbox",
+        priority = 1000,
+        config = function()
+            vim.o.background = "dark"
+            vim.cmd([[colorscheme gruvbox]])
+        end
+    },
 }
---
+++ lua/krish/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    opts = {
+        size = 20,
+        open_mapping = [[<c-\>]],
+        direction = "float",
+        shading_factor = 2,
+    }
+}
