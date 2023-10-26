8897123a092f2176ce63fee1e8071600809c5022
Krishna Khong <krishna@khong.dev>
2023-10-26 09:19:57 +0800

    disable italics on strings and comments

--- lua/krish/plugins/colorscheme.lua
+++ lua/krish/plugins/colorscheme.lua
@@ -4,6 +4,22 @@ return {
         name = "gruvbox",
         priority = 1000,
         config = function()
+            local gruvbox = require("gruvbox")
+
+            gruvbox.setup({
+                terminal_colors = true,
+                undercurl = true,
+                underline = true,
+                bold = true,
+                italic = {
+                    strings = false,
+                    emphasis = true,
+                    comments = false,
+                    operators = false,
+                    folds = true,
+                },
+            })
+
             vim.o.background = "dark"
             vim.cmd([[colorscheme gruvbox]])
         end
