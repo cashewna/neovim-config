5ba8020920c343c74a8ebc3225fce79cf0783555
Krishna Khong <krishnakhong@gmail.com>
2023-10-27 14:22:02 +1100

    feat: disable italics in some cases

--- lua/krish/plugins/colorscheme.lua
+++ lua/krish/plugins/colorscheme.lua
@@ -4,6 +4,15 @@ return {
         name = "gruvbox",
         priority = 1000,
         config = function()
+            require("gruvbox").setup({
+                italic = {
+                    strings = false,
+                    emphasis = true,
+                    comments = false,
+                    operators = false,
+                    folds = true,
+                }
+            })
             vim.o.background = "dark"
             vim.cmd([[colorscheme gruvbox]])
         end
