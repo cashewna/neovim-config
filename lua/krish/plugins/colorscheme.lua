5d6d55bc5c5d54b295619c02a94c40c5e864296c
Krishna Khong <krishna@khong.dev>
2023-12-14 01:24:55 +0800

    feat: change colorscheme to kanagawa-wave

--- lua/krish/plugins/colorscheme.lua
+++ lua/krish/plugins/colorscheme.lua
@@ -1,20 +1,13 @@
 return {
     {
-        "ellisonleao/gruvbox.nvim",
-        name = "gruvbox",
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
         priority = 1000,
         config = function()
-            require("gruvbox").setup({
-                italic = {
-                    strings = false,
-                    emphasis = true,
-                    comments = false,
-                    operators = false,
-                    folds = true,
-                }
+            require("kanagawa").setup({
+                commentStyle = { italic = false },
             })
-            vim.o.background = "dark"
-            vim.cmd([[colorscheme gruvbox]])
+            vim.cmd([[colorscheme kanagawa-wave]])
         end
     },
 }
