f30d56d235f21f1d81372450b2ad7755fc014182
Krishna Khong <krishnakhong@gmail.com>
2024-02-16 23:44:07 +0800

    feat: change colorscheme to catpuccin

--- lua/krish/plugins/colorscheme.lua
+++ lua/krish/plugins/colorscheme.lua
@@ -1,13 +1,17 @@
 return {
-    {
-        "rebelot/kanagawa.nvim",
-        name = "kanagawa",
-        priority = 1000,
-        config = function()
-            require("kanagawa").setup({
-                commentStyle = { italic = false },
-            })
-            vim.cmd([[colorscheme kanagawa-wave]])
-        end
-    },
+    "catppuccin/nvim", name = "catppuccin", priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            background = {
+                light = "latte",
+                dark = "mocha",
+            },
+            no_italic = false,
+            integrations = {
+                cmp = true,
+                treesitter = true,
+            },
+        })
+        vim.cmd('colorscheme catppuccin')
+    end
 }
