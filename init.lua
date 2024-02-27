52fb5da84a4aa5c4cef72b54ee64ee2d22be1e3e
Krishna Khong <krishnakhong@gmail.com>
2024-02-27 13:23:31 +0800

    add support for mips

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
 require("krish.core")
 require("krish.lazy")
+vim.cmd("source ~/.config/nvim/syntax/mips.vim")
--- lua/krish/core/options.lua
+++ lua/krish/core/options.lua
@@ -4,9 +4,9 @@ opt.relativenumber = true
 opt.number = true
 
 -- Tabs & indentation
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.expandtab = true
+opt.tabstop = 8
+opt.shiftwidth = 8
+opt.softtabstop = 8
 opt.autoindent = true
 
 opt.colorcolumn = "80"
