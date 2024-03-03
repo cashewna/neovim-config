c7e20e079ab167c721716927bacee90d095b3728
Krishna Khong <krishnakhong@gmail.com>
2024-03-03 14:29:12 +0800

    bump and add nvim-surround

--- lua/krish/core/options.lua
+++ lua/krish/core/options.lua
@@ -4,9 +4,8 @@ opt.relativenumber = true
 opt.number = true
 
 -- Tabs & indentation
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.expandtab = true
+opt.shiftwidth = 2
 opt.autoindent = true
+opt.softtabstop = 4
 
 opt.colorcolumn = "80"
