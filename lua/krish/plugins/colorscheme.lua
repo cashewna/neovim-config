f3ba55ce0b953909e2678014955451d21a76ba8a
Krishna Khong <krishnakhong@gmail.com>
2024-07-06 04:56:25 +1000

    update colorscheme to ayu-mirage

--- lua/krish/plugins/colorscheme.lua
+++ lua/krish/plugins/colorscheme.lua
@@ -1,4 +1,15 @@
 return {
+    "Shatur/neovim-ayu", name = "ayu",
+    config = function()
+      require('ayu').setup({
+	mirage = true, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+	terminal = true, -- Set to `false` to let terminal manage its own colors.
+	overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+      })
+      vim.cmd('colorscheme ayu-mirage')
+    end
+}
+--[[ return {
     "catppuccin/nvim", name = "catppuccin", priority = 1000,
     config = function()
         require("catppuccin").setup({
@@ -14,4 +25,4 @@ return {
         })
         vim.cmd('colorscheme catppuccin')
     end
-}
+} ]]--
